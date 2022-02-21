9faaf6df095656552bf764518c06c1336a5f88c4
ViktoriA <viktoriya1008@gmail.com>
2022-02-21 22:56:40 +0700

    Обновленная сделанная домашка

---
+++ src/Homework1/Obstacles/Course.java
@@ -0,0 +1,20 @@
+package Homework1.Obstacles;
+
+import Homework1.Players.Member;
+import Homework1.Players.Team;
+
+public class Course {
+    private String trackName;
+    private Obstacles[] obstacles;
+
+    public Course(String trackName, Obstacles[] obstacles) {
+        this.trackName = trackName;
+        this.obstacles = obstacles;
+    }
+
+    public void doIT(Team team) {
+        for (int i = 0; i < obstacles.length; i++) {
+            team.passAnObstacle(this.obstacles[i]);
+        }
+    }
+}
---
+++ src/Homework1/WorkClass.java
@@ -0,0 +1,42 @@
+package Homework1;
+
+import Homework1.Obstacles.Course;
+import Homework1.Obstacles.Obstacles;
+import Homework1.Players.Member;
+import Homework1.Players.Team;
+
+public class WorkClass {
+    public static void main(String args[]){
+        Member player1 = new Member("Ivan", 10, 5, 3);
+        Member player2 = new Member("Ira", 5, 3, 2);
+        Member player3 = new Member("Roma", 14, 10, 6);
+        Member player4 = new Member("Liza", 6, 4, 3);
+
+        Member player5 = new Member("Dima", 20, 9, 5);
+        Member player6 = new Member("Gleb", 10, 8, 3);
+        Member player7 = new Member("Kirill", 16, 10, 6);
+        Member player8 = new Member("Boris", 11, 5, 3);
+
+        Member[] membersTeam1 = {player1, player2, player3, player4};
+        Member[] membersTeam2 = {player5, player6, player7, player8};
+
+        Team team1 = new Team("Losers", membersTeam1);
+        Team team2 = new Team("Winners", membersTeam2);
+
+//_____________________________________________________________________________________
+
+        Obstacles trak1 = new Obstacles(10,5,2);
+        //Obstacles trak2 = new Obstacles(5,2,1);
+        Obstacles[] obstacles = {trak1};
+        Course course = new Course("Trak1", obstacles);
+//_______________________________________________________________________________________
+        team1.showAllMembers();
+        System.out.println("______________");
+        course.doIT(team1);
+        System.out.println("*********************");
+        team2.showAllMembers();
+        System.out.println("______________");
+        course.doIT(team2);
+    }
+
+}
