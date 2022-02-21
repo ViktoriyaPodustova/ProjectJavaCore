cb222b68339041fdefba8366861c033ff3c339df
ViktoriA <viktoriya1008@gmail.com>
2022-02-21 22:15:37 +0700

    Homework1

---
+++ src/Homework1/Obstacles/Obstacles.java
@@ -0,0 +1,18 @@
+package Homework1.Obstacles;
+
+public class Obstacles {
+    private int runDistance;
+    private int swimDistance;
+    private int jumpDistance;
+
+    public Obstacles(int runDistance, int swimDistance, int jumpDistance) {
+        this.runDistance = runDistance;
+        this.swimDistance = swimDistance;
+        this.jumpDistance = jumpDistance;
+    }
+
+public int getRunDistance(){return runDistance;}
+    public int getSwimDistance(){return swimDistance;}
+    public int getJumpDistance(){return jumpDistance;}
+
+}
---
+++ src/Homework1/Players/Member.java
@@ -0,0 +1,25 @@
+package Homework1.Players;
+
+public class Member {
+    public String name;
+    private int maxRun;
+    private int maxSwim;
+    private int maxJump;
+
+    public Member(String name, int maxRun, int maxSwim, int maxJump) {
+        this.name = name;
+        this.maxRun = maxRun;
+        this.maxSwim = maxSwim;
+        this.maxJump = maxJump;
+    }
+    public String getName(){
+        return name;
+    }
+    public int getMaxRun(){return maxRun;}
+    public int getMaxSwim(){return maxSwim;}
+    public int getMaxJump(){return maxJump;}
+
+
+
+
+}
